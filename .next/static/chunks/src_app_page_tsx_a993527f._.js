a8a6312a009de304e0884c84f11fe41ceb724b22
nagitta <naggitaaaa@gmail.com>
2025-07-29 14:48:19 +0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753775300509

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_e7d97d37._.js",
+  "static/chunks/node_modules_ad443dba._.js"
+],
+    source: "dynamic"
+});
